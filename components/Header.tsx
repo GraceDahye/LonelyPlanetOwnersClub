d88fb091e248baae1025f57cc646fa602e9dbcd5
Dahye Lee <dhl@Dahyeui-MacBook-Air.local>
2022-07-23 22:20:33 +0900

    “20220723-4”

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,7 +31,7 @@ const Header: FC = () => {
       shadow="md"
       zIndex={1}
       flexDir={["column", "column", "row"]}
-      bgColor={colorMode === "light" ? “purple” : “purple”}
+      bgColor="purple"
     >
       <Box fontWeight="bold" fontSize="lg" cursor="pointer">
         <Link href="/">
