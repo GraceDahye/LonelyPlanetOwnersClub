1efeccea3a370224d62b5415acdcec1676c5fb10
Dahye Lee <dhl@Dahyeui-MacBook-Air.local>
2022-07-23 20:46:15 +0900

    20220723

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,7 +4,6 @@ import { Link as ScrollLink } from "react-scroll";
 
 // @ 타이틀 이미지는 이미지에 마우스를 올리면 Gif 이미지가 나오도록 하였습니다. public/images 교체하셔서 사용하시면 됩니다.
 const titleImage = "title.png";
-const titleImageGif = "title.gif";
 
 const Title: FC = () => {
   const [isHover, setIsHover] = useState<boolean>(false);
@@ -29,7 +28,7 @@ const Title: FC = () => {
           onMouseLeave={() => setIsHover(false)}
           alt="title"
         />
-        <Image src={`../images/${titleImageGif}`} alt="title" />
+      
       </Box>
       <ScrollLink to="Story" spy={true} smooth={true}>
         <Button
